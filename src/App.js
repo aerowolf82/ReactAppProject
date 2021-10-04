d78762a366c0fdf5141b07191364a36f1528616b
Andrew Clapp <aerowolf82@gmail.com>
2021-10-04 17:55:44 -0400

    start of Name

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
+import Name from './Components/charName.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Rick And Morty</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Name />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +21,8 @@ function App() {
           Learn React
         </a>
       </header>
+      {/* <JokeList selectedJoke={selectedJoke} jokeSelectorFunc={selectJoke} list={jokeList} /> */}
+ 
     </div>
   );
 }
